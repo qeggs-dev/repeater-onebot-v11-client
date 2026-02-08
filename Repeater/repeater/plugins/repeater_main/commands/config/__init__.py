ad0da49975871b84a5c107ca7678dc48e3a3bb83
Qeggs <get_boot@outlook.com>
2026-02-08 14:37:32 +0800

    feat(config): 添加自定义名称设置功能
    新增 handle_set_custom_name 命令处理函数用于设置自定义名称，
    并在 __all__ 中导出该功能以支持配置模块中的自定义名称功能。

--- Repeater/repeater/plugins/repeater_main/commands/config/__init__.py
+++ Repeater/repeater/plugins/repeater_main/commands/config/__init__.py
@@ -7,6 +7,7 @@ from ._data_command.set_frequency_penalty import handle_set_frequency_penalty
 from ._data_command.set_presence_penalty import handle_set_presence_penalty
 from ._data_command.change_default_personality import handle_change_default_personality
 from ._data_command.cross_user_data_access import handle_cross_user_data_access
+from ._data_command.custom_name import handle_set_custom_name
 from ._data_command.new_requests_text_only import handle_new_requests_text_only
 from ._data_command.set_html_template import handle_set_html_template
 from ._data_command.set_render_style import handle_set_render_style
@@ -38,6 +39,7 @@ __all__ = [
     "handle_set_presence_penalty",
     "handle_change_default_personality",
     "handle_cross_user_data_access",
+    "handle_set_custom_name",
     "handle_new_requests_text_only",
     "handle_set_html_template",
     "handle_del_config",
