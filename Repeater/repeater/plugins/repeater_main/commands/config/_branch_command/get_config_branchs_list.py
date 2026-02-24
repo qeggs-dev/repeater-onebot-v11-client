8930220714268f68f7ff94c464c9c8f1fcd11919
Qeggs <get_boot@outlook.com>
2026-02-24 15:42:04 +0800

    feat(Repeater): 添加配置分支列表查询命令
    新增 getConfigBranchsList 命令用于获取配置分支列表，
    支持别名 gcfgbl、get_config_branchs_list 等多种形式调用。
    该功能允许用户查询可用的配置分支并以结构化方式显示结果。

---
+++ Repeater/repeater/plugins/repeater_main/commands/config/_branch_command/get_config_branchs_list.py
@@ -0,0 +1,43 @@
+from nonebot import on_command
+from nonebot.rule import to_me
+from nonebot.params import CommandArg
+from nonebot.adapters import Message
+from nonebot.adapters.onebot.v11 import MessageEvent, MessageSegment
+from nonebot.adapters import Bot
+
+from ..._clients import ConfigCore
+from ....assist import PersonaInfo, SendMsg
+
+get_config_branchs_list = on_command("getConfigBranchsList", aliases={"gcfgbl", "get_config_branchs_list", "Get_Config_Branchs_List", "GetConfigBranchsList"}, rule=to_me(), block=True)
+
+@get_config_branchs_list.handle()
+async def handle_config_branchs_list(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
+    persona_info = PersonaInfo(bot=bot, event=event, args=args)
+    send_msg = SendMsg("Config.Get_Config_Branchs_List", get_config_branchs_list, persona_info)
+
+    if send_msg.is_debug_mode:
+        await send_msg.send_debug_mode()
+    
+    config_core = ConfigCore(persona_info)
+    if send_msg.is_debug_mode:
+        await send_msg.send_debug_mode()
+    else:
+        response = await config_core.get_branch_list()
+        if response.code == 200:
+            data = response.json()
+            if not isinstance(data, list):
+                await send_msg.send_error("Unable to process data.")
+
+            text_buffer: list[str] = []
+            text_buffer.append(f"Branch Type: Config")
+            text_buffer.append(f"User Name: {persona_info.display_name}")
+            if data:
+                text_buffer.append("Branchs:")
+                for branch_id in data:
+                    text_buffer.append(f"  - {branch_id}")
+            else:
+                text_buffer.append("No branchs found.")
+
+            await send_msg.send_check_length_prompt("\n".join(text_buffer))
+        else:
+            await send_msg.send_response_check_code(response, "Get Config branch list failed")
