a88c087c47d1e95b4641c2c70e664e422d1cf640
Qeggs <get_boot@outlook.com>
2026-03-03 08:39:37 +0800

    feat(nexus): 添加环境上传下载到Nexus的功能
    - 新增环境上传到Nexus命令，支持设置超时时间
    - 新增从Nexus下载环境命令，支持UUID验证
    - 实现Nexus核心客户端接口调用
    - 添加命令别名支持，包括英文和驼峰命名方式
    - 完善错误处理和响应码检查机制

---
+++ Repeater/repeater/plugins/repeater_main/commands/nexus/__init__.py
@@ -0,0 +1,7 @@
+from ._upload_environment_to_nexus import handle_environment_upload_to_nexus
+from ._download_environment_from_nexus import handle_environment_download_from_nexus
+
+__all__ = [
+    "handle_environment_upload_to_nexus",
+    "handle_environment_download_from_nexus"
+]
---
+++ Repeater/repeater/plugins/repeater_main/commands/nexus/_upload_environment_to_nexus.py
@@ -0,0 +1,44 @@
+from nonebot import on_command
+from nonebot.rule import to_me
+from nonebot.params import CommandArg
+from nonebot.adapters import Message
+from nonebot.adapters.onebot.v11 import MessageEvent, MessageSegment
+from nonebot.adapters import Bot
+
+from .._clients import NexusCore
+from ...assist import PersonaInfo, SendMsg
+
+environment_upload_to_nexus = on_command("environmentUploadToNexus", aliases={"eutn", "environment_upload_to_nexus", "Environment_Upload_To_Nexus", "EnvironmentUploadToNexus"}, rule=to_me(), block=True)
+
+@environment_upload_to_nexus.handle()
+async def handle_environment_upload_to_nexus(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
+    persona_info = PersonaInfo(bot=bot, event=event, args=args)
+    send_msg = SendMsg("Nexus.Upload_Environment_To_Nexus", environment_upload_to_nexus, persona_info)
+
+    if send_msg.is_debug_mode:
+        await send_msg.send_debug_mode()
+    
+    nexus_core = NexusCore(persona_info)
+
+    timeout = None
+    if persona_info.message_str:
+        try:
+            timeout = int(persona_info.message_str)
+        except ValueError:
+            await send_msg.send_error("Invalid timeout value")
+
+    if send_msg.is_debug_mode:
+        await send_msg.send_debug_mode()
+    else:
+        response = await nexus_core.upload_environment_to_nexus(timeout)
+
+        if response.code == 200:
+            data = response.get_data()
+            if data is None:
+                await send_msg.send_error("Unable to process data.")
+            else:
+                await send_msg.send_prompt(
+                    f"Upload successful.\nFile ID: {data.file_uuid}"
+                )
+        else:
+            await send_msg.send_response_check_code(response, "Unable to upload environment to Nexus.")
