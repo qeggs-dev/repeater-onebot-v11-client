2224fbe7d8e47e3a5a5639395520e196d56f077a
Qeggs <get_boot@outlook.com>
2026-03-10 02:19:25 +0800

    feat(Repeater): 修改Nexus响应模型的数据类型定义
    - 将NexusUploadResponse和NexusDownloadResponse中的nexus_message字段
      类型从str修改为str | Any，以支持更灵活的数据类型
    - 添加typing.Any导入以支持联合类型注解

--- Repeater/repeater/plugins/repeater_main/commands/_clients/_data_manager/_nexus_response.py
+++ Repeater/repeater/plugins/repeater_main/commands/_clients/_data_manager/_nexus_response.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel
+from typing import Any
 
 class NexusUploadResponse(BaseModel):
     message: str = ""
-    nexus_message: str = ""
+    nexus_message: str | Any = ""
     file_uuid: str | None = None
 
 class NexusDownloadResponse(BaseModel):
     message: str = ""
-    nexus_message: str = ""
+    nexus_message: str | Any = ""
