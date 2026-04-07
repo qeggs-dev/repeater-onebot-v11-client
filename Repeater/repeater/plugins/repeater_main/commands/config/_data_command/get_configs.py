cc4932dfc5c7b2ccbdb1f744f7b967a3b96b8ba2
Qeggs <get_boot@outlook.com>
2026-04-07 19:45:59 +0800

    feat(config): 添加获取配置命令功能
    新增了 get_configs 命令处理函数，支持以 JSON 和 YAML 格式导出配置信息。
    该功能允许用户通过 getConfigs/gcfg/get_configs 等别名命令获取当前系统配置，
    并可选择输出格式为 JSON 或 YAML 格式。

---
+++ Repeater/repeater/plugins/repeater_main/commands/config/_data_command/get_configs.py
@@ -0,0 +1,42 @@
+import json
+import yaml
+
+from nonebot import on_command
+from nonebot.rule import to_me
+from nonebot.params import CommandArg
+from nonebot.adapters import Message
+from nonebot.adapters.onebot.v11 import MessageEvent
+from nonebot.adapters import Bot
+
+from ..._clients import ConfigCore
+from ....assist import PersonaInfo, SendMsg
+from enum import StrEnum
+
+get_configs = on_command("getConfigs", aliases={"gcfg", "get_configs", "Get_Configs", "GetConfigs"}, rule=to_me(), block=True)
+
+class FormatType(StrEnum):
+    JSON = "json"
+    YAML = "yaml"
+
+@get_configs.handle()
+async def handle_get_configs(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
+    persona_info = PersonaInfo(bot=bot, event=event, args=args)
+    send_msg = SendMsg("Config.Get_Configs", persona_info, get_configs, None)
+
+    if send_msg.is_debug_mode:
+        await send_msg.send_debug_mode()
+    try:
+        format_type = FormatType(persona_info.message_striped_str.lower())
+    except ValueError:
+        await send_msg.send_error("Format type is not supported. Please use 'json' or 'yaml'.")
+    
+    config_core = ConfigCore(persona_info)
+    response = await config_core.get_configs()
+    if response:
+        match format_type:
+            case FormatType.JSON:
+                await send_msg.send_check_length_prompt(json.dumps(response, ensure_ascii=False, indent=4))
+            case FormatType.YAML:
+                await send_msg.send_check_length_prompt(yaml.dump(response, allow_unicode=True))
+    else:
+        await send_msg.send_response(response, "Failed to get configs.")
