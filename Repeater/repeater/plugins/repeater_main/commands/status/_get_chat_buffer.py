3a2d1fb80ce8a2e5b7043f6a7864c4b03cec7191
Qeggs <get_boot@outlook.com>
2026-04-07 17:55:21 +0800

    feat(status): 添加获取聊天缓冲区功能
    新增_get_chat_buffer模块实现获取聊天缓冲区命令
    支持通过getChatBuffer、gcb、get_chat_buffer等别名调用
    集成到状态命令模块中，完善功能列表

---
+++ Repeater/repeater/plugins/repeater_main/commands/status/_get_chat_buffer.py
@@ -0,0 +1,32 @@
+from nonebot import on_command
+from nonebot.rule import to_me
+from nonebot.params import CommandArg
+from nonebot.adapters import Message
+from nonebot.adapters.onebot.v11 import MessageEvent
+from nonebot.adapters import Bot
+from typing import Any
+
+from .._clients import ChatCore
+from ...assist import PersonaInfo, SendMsg, str_to_bool
+
+get_chat_buffer = on_command("getChatBuffer", aliases={"gcb", "get_chat_buffer", "Get_Chat_Buffer", "GetChatBuffer"}, rule=to_me(), block=True)
+
+@get_chat_buffer.handle()
+async def handle_get_chat_buffer(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
+    persona_info = PersonaInfo(bot=bot, event=event, args=args)
+    send_msg = SendMsg("Status.Get_Chat_Buffer", get_chat_buffer, persona_info)
+
+    if send_msg.is_debug_mode:
+        await send_msg.send_debug_mode()
+    else:
+        chat_core = ChatCore()
+        response = await chat_core.get_chat_buffer()
+        if response:
+            buffer = response.get_data()
+            if buffer is None:
+                await send_msg.send_error(response.get_error())
+            else:
+                await send_msg.send_chat_response(
+                    reasoning_content = buffer.reasoning,
+                    content = buffer.content
+                )
