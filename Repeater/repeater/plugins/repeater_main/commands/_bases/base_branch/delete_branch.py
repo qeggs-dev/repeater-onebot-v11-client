e0174e54e9ce4e6ff0df17a595e0510ae19dc154
Qeggs <get_boot@outlook.com>
2026-04-22 00:50:26 +0800

    fix(repeater_main): 修复删除分支消息中的大小写错误
    将响应消息中的"Active branch"改为"active branch"，
    保持一致的小写格式以符合应用的消息显示规范。

--- Repeater/repeater/plugins/repeater_main/commands/_bases/base_branch/delete_branch.py
+++ Repeater/repeater/plugins/repeater_main/commands/_bases/base_branch/delete_branch.py
@@ -8,4 +8,4 @@ from ..._clients import UserDataClient
 class DeleteBranch(BaseBranch):
     async def parser(self, branch_id: str, client: UserDataClient, send_msg: SendMsg):
         response = await client.delete()
-        await send_msg.send_response_check_code(response, f"Deleted Active branch")
+        await send_msg.send_response_check_code(response, f"Deleted active branch")
