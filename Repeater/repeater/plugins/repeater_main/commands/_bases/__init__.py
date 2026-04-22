12fea933b732712efcd15b2d3c69adc0fbab14de
Qeggs <get_boot@outlook.com>
2026-04-22 17:39:04 +0800

    refactor(commands): 重命名聊天基础包文件
    将 chat_base_pkg.py 重命名为 base_chat_pkg.py，
    以保持命名约定的一致性。

--- Repeater/repeater/plugins/repeater_main/commands/_bases/__init__.py
+++ Repeater/repeater/plugins/repeater_main/commands/_bases/__init__.py
@@ -1,4 +1,4 @@
-from .chat_base_pkg import BaseChat
+from .base_chat_pkg import BaseChat
 from .base_config_pkg import BaseConfig, OperationType
 from .base_branch import *
 from .base_nexus import *
