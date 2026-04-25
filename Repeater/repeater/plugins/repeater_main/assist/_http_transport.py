6a6163eb3c597c73c07ccddccc12fd72a96297fa
Qeggs <get_boot@outlook.com>
2026-04-25 16:22:02 +0800

    feat(repeater_main): 改进HTTP请求日志格式
    在请求日志中添加换行符，使cURL命令输出更加清晰易读，
    便于调试和查看请求详情

--- Repeater/repeater/plugins/repeater_main/assist/_http_transport.py
+++ Repeater/repeater/plugins/repeater_main/assist/_http_transport.py
@@ -7,7 +7,7 @@ class HTTPTransport(AsyncHTTPTransport):
     async def handle_async_request(self, request: Request) -> Response:
         curlify = curlify2.Curlify(request)
         logger.info(
-            "Sending request: {curl_str}",
+            "Sending request: \n{curl_str}\n",
             curl_str = curlify.to_curl(),
         )
         return await super().handle_async_request(request)
