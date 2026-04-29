4eb0901ef3826cb334060d67e0bf6280b13b954b
Qeggs <get_boot@outlook.com>
2026-04-30 06:57:25 +0800

    refactor(repeater_main): 移除未使用的模型类型导出
    移除了 _model_types 模块中的 ModelType 和 MODEL_TYPES 导出，
    因为它们在当前模块中不再被使用。

--- Repeater/repeater/plugins/repeater_main/commands/_clients/_model_info/__init__.py
+++ Repeater/repeater/plugins/repeater_main/commands/_clients/_model_info/__init__.py
@@ -1,14 +1,8 @@
-from ._model_types import (
-    ModelType,
-    MODEL_TYPES
-)
 from ._model_info import ModelInfo
 from ._model_info_client import ModelInfoClient
 from ._models import ModelsResponse
 
 __all__ = [
-    "ModelType",
-    "MODEL_TYPES",
     "ModelInfo",
     "ModelInfoClient",
     "ModelsResponse"
