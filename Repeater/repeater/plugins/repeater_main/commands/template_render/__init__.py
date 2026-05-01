8887f9a0127c2ba4ed0785ad5f8e83c24e42477f
Qeggs <get_boot@outlook.com>
2026-05-02 03:06:51 +0800

    feat(repeater_main): 添加模板渲染命令功能
    新增 templateRender、templateRenderText 和 templateRenderImage 命令，
    用于处理模板变量扩展和渲染功能。支持多种别名以便兼容性使用，
    并提供文本和图片两种渲染模式。

---
+++ Repeater/repeater/plugins/repeater_main/commands/template_render/__init__.py
@@ -0,0 +1,9 @@
+from ._template_render import TemplateRender
+from ._template_render_text import TemplateRenderText
+from ._template_render_image import TemplateRenderImage
+
+__all__ = [
+    "TemplateRender",
+    "TemplateRenderText",
+    "TemplateRenderImage",
+]
---
+++ Repeater/repeater/plugins/repeater_main/commands/template_render/_template_render.py
@@ -0,0 +1,43 @@
+from ...assist import PersonaInfo, SendMsg, Response
+from ...command_register import CommandCaller, CommandPackage
+from .._clients import TemplateRenderClient
+
+@CommandCaller.register
+class TemplateRender(CommandPackage):
+    cmd = "templateRender"
+    aliases = {
+        "tr",
+        "TR",
+        "template_render",
+        "Template_Render",
+        "TemplateRender",
+        "TEMPLATE_RENDER",
+
+        # Reserved for compatibility.
+        "varExpand",
+        "ve",
+        "VE",
+        "var_expand",
+        "Var_Expand",
+        "VarExpand",
+        "VAR_EXPAND",
+    }
+
+    @property
+    def component(self) -> str:
+        return f"Template.Render.{self.__class__.__name__}"
+
+    async def handler(self, persona_info: PersonaInfo, send_msg: SendMsg):
+        if send_msg.is_debug_mode:
+            await send_msg.send_debug_mode()
+
+        msg = persona_info.message_striped_str
+        variable_expansion_client = TemplateRenderClient(persona_info)
+        response = await variable_expansion_client.render(text = msg)
+        await self.send_result(persona_info, send_msg, response)
+    
+    async def send_result(self, persona_info: PersonaInfo, send_msg: SendMsg, response: Response[None]):
+        if response.code == 200:
+            await send_msg.send_check_length(response.text)
+        else:
+            await send_msg.send_response_check_code(response)
