cac5baed1699745786f262f3bb8f82a12a9c7c9d
Qeggs <get_boot@outlook.com>
2026-05-14 01:42:44 +0800

    refactor(SSL): 将SSLContext改为实例化对象
    将SSLContext类从静态方法实现改为实例方法实现，
    移除了ClassVar类型注解，改为在__init__方法中初始化ssl_context属性，
    使SSL上下文管理更加面向对象和灵活。

--- Repeater/repeater/plugins/repeater_main/assist/_ssl.py
+++ Repeater/repeater/plugins/repeater_main/assist/_ssl.py
@@ -1,21 +1,18 @@
 import ssl
-from typing import ClassVar
 
 class SSLContext:
-    ssl_context: ClassVar[ssl.SSLContext] = ssl.create_default_context()
+    def __init__(self):
+        self.ssl_context: ssl.SSLContext = ssl.create_default_context()
 
-    @classmethod
-    def get_ssl_context(cls):
-        return cls.ssl_context
+    def get_ssl_context(self):
+        return self.ssl_context
     
-    @classmethod
-    def set_ssl_context(cls, ssl_context: ssl.SSLContext):
-        cls.ssl_context = ssl_context
+    def set_ssl_context(self, ssl_context: ssl.SSLContext):
+        self.ssl_context = ssl_context
     
-    @classmethod
-    def create_default_context(cls):
-        cls.ssl_context = ssl.create_default_context()
-        return cls.ssl_context
+    def create_default_context(self):
+        self.ssl_context = ssl.create_default_context()
+        return self.ssl_context
 
 ssl_context = SSLContext()
 
