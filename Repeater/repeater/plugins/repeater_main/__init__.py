192d7843d387883291dd4b245225fa9b75f49953
Qeggs <get_boot@outlook.com>
2026-05-14 00:45:47 +0800

    refactor(repeater_main): 将命令导入重构到独立模块
    将所有命令相关的导入语句从主模块中移除，
    并将它们移动到新的 _import_plugins.py 模块中，
    以提高代码组织结构和可维护性。
    
    新增了统计功能相关的导入项。

--- Repeater/repeater/plugins/repeater_main/__init__.py
+++ Repeater/repeater/plugins/repeater_main/__init__.py
@@ -4,63 +4,7 @@ from nonebot import logger as _logger
 start_import_time = _time.perf_counter_ns()
 
 try:
-    # === Chat === #
-    from .commands.chat import *
-
-    # === Context === #
-    from .commands.context import *
-
-    # === Prompt === #
-    from .commands.prompt import *
-
-    # === Config === #
-    from .commands.config import *
-
-    # === Mixed === #
-    from .commands.mixed import *
-
-    # === Model Info === #
-    from .commands.model_info import *
-
-    # === Status === #
-    from .commands.status import *
-
-    # === Licenses === #
-    from .commands.licenses import *
-
-    # === More Interesting Tools === #
-    from .commands.more_interesting_tools import *
-
-    # === Nexus === #
-    from .commands.nexus import *
-
-    # === Var Expand === #
-    from .commands.template_render import *
-
-    # === UserDataFile === #
-    from .commands.user_file import *
-
-    # === SessionID === #
-    from .commands.get_namespace import GetNamespace
-
-    # === TextRender === #
-    from .commands.markdown_render import TextRender
-
-    # === Comment === #
-    from .commands.annotation import Annotation
-
-    # === Adaptation === #
-    from .commands.adaptation import AdaptationInfo
-
-    # === Calculate Length Score === #
-    from .commands.calculate_length_score import CalculateLengthScore
-
-    # === Send Any Message === #
-    from .commands.send_msg import SendMessage
-
-    # === Adaptation Info === #
-    from ._adaptation_info import __adaptation__, __adaptation_text__
-
+    from ._import_plugins import *
 finally:
     end_import_time = _time.perf_counter_ns()
     _logger.info(
