fab87e7d6be76cc3950e4c6622f843809e136ccf
Qeggs <get_boot@outlook.com>
2026-05-26 23:21:11 +0800

    refactor(repeater_main): 移除未使用的 SendMsgLimitType 导入
    移除了 _client_net_configs/_storage_configs 模块中已弃用的
    SendMsgLimitType 类的导入声明。

--- Repeater/repeater/plugins/repeater_main/client_net_configs/_storage_configs/__init__.py
+++ Repeater/repeater/plugins/repeater_main/client_net_configs/_storage_configs/__init__.py
@@ -1,6 +1,5 @@
 from ._storage_configs import StorageConfigs
 from ._camouflage import Camouflage
-from ._send_msg_limit_type import SendMsgLimitType
 from ._server_api_timeout import ServerAPITimeout
 from ._storage_configs_instance import storage_configs
 from ._text_length_score_configs import TextLengthScoreConfigs
@@ -9,7 +8,6 @@ from ._text_length_score_threshold import TextLengthScoreThreshold
 __all__ = [
     "StorageConfigs",
     "Camouflage",
-    "SendMsgLimitType",
     "ServerAPITimeout",
     "storage_configs",
     "TextLengthScoreConfigs",
