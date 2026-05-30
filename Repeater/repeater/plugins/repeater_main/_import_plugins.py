892f0cba8766f68f70448ffb1538f9452ad4ab87
Qeggs <get_boot@outlook.com>
2026-05-30 16:55:30 +0800

    feat(repeater_main): 添加命令调用器注册功能
    - 引入CommandCaller类用于命令注册管理
    - 在插件初始化时记录已注册的命令信息
    - 增强插件系统的命令管理能力

--- Repeater/repeater/plugins/repeater_main/_import_plugins.py
+++ Repeater/repeater/plugins/repeater_main/_import_plugins.py
@@ -3,3 +3,7 @@ from .commands import *
 
 # === Adaptation Info === #
 from ._adaptation_info import __adaptation__, __adaptation_text__
+
+from .command_register import CommandCaller as _CommandCaller
+
+_CommandCaller.log_registed_info()
