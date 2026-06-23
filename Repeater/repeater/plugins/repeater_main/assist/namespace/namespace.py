1520f4959af2c794770622b7114a7a55dd26cb77
Qeggs <get_boot@outlook.com>
2026-06-23 20:09:00 +0800

    feat(repeater_main): 修改命名空间模型字段类型
    将Namespace模型中的group_id从int类型改为str类型，
    user_id从int类型改为str类型，并相应地将默认值
    从0改为""以匹配新的数据类型

--- Repeater/repeater/plugins/repeater_main/assist/namespace/namespace.py
+++ Repeater/repeater/plugins/repeater_main/assist/namespace/namespace.py
@@ -13,8 +13,8 @@ class Namespace(BaseModel):
     )
 
     mode: MessageSource = MessageSource.GROUP
-    group_id: int | None = None
-    user_id: int = 0
+    group_id: str | None = None
+    user_id: str = ""
 
     @property
     def _namespace(self) -> str:
