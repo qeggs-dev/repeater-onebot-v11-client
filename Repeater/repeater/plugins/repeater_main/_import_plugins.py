41917f503181054d6a035c81fb4361d93286a182
Qeggs <get_boot@outlook.com>
2026-06-25 15:22:16 +0800

    feat(repeater_main): 添加日志警告处理功能
    - 导入新的警告处理器模块
    - 注册警告处理器到命令调用器中
    - 实现警告处理器的注入机制

--- Repeater/repeater/plugins/repeater_main/_import_plugins.py
+++ Repeater/repeater/plugins/repeater_main/_import_plugins.py
@@ -9,4 +9,7 @@ from ._adaptation_info import __adaptation__, __adaptation_text__
 
 from .command_register import CommandCaller as _CommandCaller
 
+# === Log Warn === #
+from ._log_warn import warning_handler
+
 _CommandCaller.log_registed_info()
