7ad0f09dd86c5d1fbba21f78a5f7ea5389a3e0a9
Qeggs <get_boot@outlook.com>
2026-06-25 08:18:59 +0800

    refactor(Repeater): 移除重复器主模块中的子模块导入器
    移除了 repeater_main/commands/config/_data_command/__init__.py 文件中
    的 SubmoduleImporter 相关代码，包括导入、实例化、方法调用和 __all__
    属性定义。此更改简化了模块结构并消除了不必要的动态导入机制。

--- Repeater/repeater/plugins/repeater_main/commands/config/_data_command/__init__.py
+++
@@ -1,6 +0,0 @@
-from ...._submodule_importer import SubmoduleImporter as _ImportPublicPkgs
-
-_importer= _ImportPublicPkgs()
-_importer.import_pkgs()
-_importer.inject_modules()
-__all__ = _importer.all_list() # type: ignore
