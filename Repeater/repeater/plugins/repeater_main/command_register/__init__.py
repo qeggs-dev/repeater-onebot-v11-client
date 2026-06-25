6dd7cfdd0556d78eb81a59909721bee8421453f6
Qeggs <get_boot@outlook.com>
2026-06-25 08:17:39 +0800

    feat(repeater_main): 添加新的命令注册组件
    新增 RunningPackage 和 SubCmdBreaked 组件到命令注册模块，
    扩展了 __all__ 导出列表以包含这些新组件。

--- Repeater/repeater/plugins/repeater_main/command_register/__init__.py
+++ Repeater/repeater/plugins/repeater_main/command_register/__init__.py
@@ -1,9 +1,13 @@
 from .caller import CommandCaller
 from .package import CommandPackage
 from .listen_type import ListenType
+from .running_package import RunningPackage
+from .sub_cmd_breaked import SubCmdBreaked
 
 __all__ = [
     "CommandCaller",
     "CommandPackage",
-    "ListenType"
+    "ListenType",
+    "RunningPackage",
+    "SubCmdBreaked",
 ]
