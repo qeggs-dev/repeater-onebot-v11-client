dc667e3e2a8cefe7f4aab34dd46a28f3733399c3
Qeggs <get_boot@outlook.com>
2026-07-14 00:36:01 +0800

    feat(repeater_main): 添加戳一戳命令功能
    新增poke_me.py文件，实现发送戳一戳消息的功能。
    支持直接发送戳一戳或指定用户发送戳一戳。
    支持别名：Poke、POKE。

---
+++ Repeater/repeater/plugins/repeater_main/commands/control/poke_me.py
@@ -0,0 +1,33 @@
+import asyncio
+
+from ...assist import PersonaInfo, SendMsg
+from ...cmd_info import CmdTypes
+from ...command_register import(
+    CommandCaller,
+    CommandPackage
+)
+from ..._adaptation_info import __adaptation__
+
+
+@CommandCaller.register
+class PokeMe(CommandPackage):
+    cmd = "poke"
+    aliases = {
+        "Poke",
+        "POKE",
+    }
+    cmd_type = CmdTypes.CONTROL
+    documents = f"""
+    Send a poke message to the user.
+
+    Usage: 
+        /{cmd} @member
+    """
+
+    async def handler(self, persona_info: PersonaInfo, send_msg: SendMsg):
+        at_list = persona_info.at_list
+        if not at_list:
+            await send_msg.send_poke()
+        else:
+            for member in at_list:
+                await send_msg.send_poke(user_id = member)
